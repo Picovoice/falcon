73b6901970abfa58adf5e7b9a02a5d222127c320
Zhuojin Liu <liu@picovoice.ai>
2025-02-05 14:46:59 -0800

    Update demo package setup to read dependency versions from requirement file

--- demo/python/setup.py
+++ demo/python/setup.py
@@ -26,6 +26,9 @@ with open(os.path.join(os.path.dirname(__file__), 'MANIFEST.in'), 'w') as f:
 with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as f:
     long_description = f.read()
 
+with open(os.path.join(os.path.dirname(__file__), "requirements.txt"), "r") as f:
+    dependencies = f.read().strip().splitlines()
+
 setuptools.setup(
     name="pvfalcondemo",
     version="1.0.3",
@@ -36,7 +39,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Picovoice/falcon",
     packages=["pvfalcondemo"],
-    install_requires=["pvfalcon==1.0.3", "pvrecorder==1.2.4", "tabulate==0.8.10"],
+    install_requires=dependencies,
     include_package_data=True,
     classifiers=[
         "Development Status :: 5 - Production/Stable",
