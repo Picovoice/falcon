1fea7610499f4338cd5c2c787337325f57e38e95
Kwangsoo Yeo <kyeo@picovoice.ai>
2025-11-20 09:57:14 -0800

    v2.0 c changes

--- demo/c/falcon_demo.c
+++ demo/c/falcon_demo.c
@@ -1,5 +1,5 @@
 /*
-    Copyright 2019-2023 Picovoice Inc.
+    Copyright 2019-2025 Picovoice Inc.
 
     You may not use this file except in compliance with the license. A copy of
    the license is located in the "LICENSE" file accompanying this source.
@@ -93,9 +93,10 @@ int picovoice_main(int argc, char **argv) {
     const char *access_key = NULL;
     const char *model_path = NULL;
     const char *library_path = NULL;
+    const char *device = NULL;
 
     int opt;
-    while ((opt = getopt(argc, argv, "a:m:l:")) != -1) {
+    while ((opt = getopt(argc, argv, "a:m:l:y:")) != -1) {
         switch (opt) {
             case 'a':
                 access_key = optarg;
@@ -106,14 +107,21 @@ int picovoice_main(int argc, char **argv) {
             case 'l':
                 library_path = optarg;
                 break;
+            case 'd':
+                device = optarg;
+                break;
             default:
                 break;
         }
     }
 
     if (!(access_key && library_path && model_path && (optind < argc))) {
-        fprintf(stderr, "usage: -a ACCESS_KEY -m MODEL_PATH -l LIBRARY_PATH audio_path0 audio_path1 ...\n");
+        fprintf(stderr, "usage: -a ACCESS_KEY -m MODEL_PATH -l LIBRARY_PATH [-d DEVICE] audio_path0 audio_path1 ...\n");
         exit(1);
+    }
+
+    if (device == NULL) {
+        device = "cpu:1";
     }
 
     void *dl_handle = open_dl(library_path);
@@ -138,7 +146,7 @@ int picovoice_main(int argc, char **argv) {
 
     const char *(*pv_falcon_version_func)() = load_symbol(dl_handle, "pv_falcon_version");
 
-    pv_status_t (*pv_falcon_init_func)(const char *, const char *, pv_falcon_t **) =
+    pv_status_t (*pv_falcon_init_func)(const char *, const char *, const char *, pv_falcon_t **) =
             load_symbol(dl_handle, "pv_falcon_init");
     if (!pv_falcon_init_func) {
         print_dl_error("failed to load `pv_falcon_init`");
@@ -195,6 +203,7 @@ int picovoice_main(int argc, char **argv) {
     pv_status_t status = pv_falcon_init_func(
             access_key,
             model_path,
+            device,
             &falcon);
     if (status != PV_STATUS_SUCCESS) {
         fprintf(stderr, "failed to init with `%s`.\n", pv_status_to_string_func(status));
