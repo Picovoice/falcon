de9288eabf6e8ed548b7a0a2dd2b814437693805
Kwangsoo Yeo <kyeo@picovoice.ai>
2025-11-20 09:57:14 -0800

    v2.0 c changes

--- demo/c/test/test_falcon_c.py
+++ demo/c/test/test_falcon_c.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2023 Picovoice Inc.
+# Copyright 2023-2025 Picovoice Inc.
 #
 # You may not use this file except in compliance with the license. A copy of the license is located in the "LICENSE"
 # file accompanying this source.
@@ -9,10 +9,27 @@
 # specific language governing permissions and limitations under the License.
 #
 
+import os
 import os.path
 import subprocess
 import sys
 import unittest
+from parameterized import parameterized
+
+def get_test_devices(device):
+    result = list()
+
+    if device == "cpu":
+        max_threads = os.cpu_count() // 2
+        i = 1
+
+        while i <= max_threads:
+            result.append(f"cpu:{i}")
+            i *= 2
+    else:
+        result.append(device)
+
+    return result
 
 
 def get_lib_ext(platform):
@@ -24,13 +41,16 @@ def get_lib_ext(platform):
         return "so"
 
 
+devices = get_test_devices(sys.argv[3])
+
+
 class FalconCTestCase(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
         cls._access_key = sys.argv[1]
         cls._platform = sys.argv[2]
-        cls._arch = "" if len(sys.argv) != 4 else sys.argv[3]
+        cls._arch = "" if len(sys.argv) != 5 else sys.argv[4]
         cls._root_dir = os.path.join(os.path.dirname(__file__), "../../..")
 
     def _get_library_file(self):
@@ -42,12 +62,14 @@ class FalconCTestCase(unittest.TestCase):
             "libpv_falcon." + get_lib_ext(self._platform)
         )
 
-    def test_falcon(self):
+    @parameterized.expand(devices)
+    def test_falcon(self, device):
         args = [
             os.path.join(os.path.dirname(__file__), "../build/falcon_demo"),
             "-a", self._access_key,
             "-l", self._get_library_file(),
             "-m", os.path.join(self._root_dir, 'lib/common/falcon_params.pv'),
+            "-y", device,
             os.path.join(self._root_dir, 'resources/audio_samples/test.wav'),
         ]
         process = subprocess.Popen(args, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
@@ -57,7 +79,7 @@ class FalconCTestCase(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 3 or len(sys.argv) > 4:
-        print("usage: test_falcon_c.py ${AccessKey} ${Platform} [${Arch}]")
+    if len(sys.argv) < 4 or len(sys.argv) > 5:
+        print("usage: test_falcon_c.py ${AccessKey} ${Platform} ${Device} [${Arch}]")
         exit(1)
-    unittest.main(argv=sys.argv[:1])
+    unittest.main(argv=sys.argv[:1], verbosity=2)
