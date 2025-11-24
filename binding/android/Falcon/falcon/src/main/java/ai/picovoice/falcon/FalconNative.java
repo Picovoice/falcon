df11bea32799b7ff321019373c97f4ea8be7a755
Kwangsoo Yeo <kyeo@picovoice.ai>
2025-11-24 12:06:30 -0800

    v2.0 android changes

--- binding/android/Falcon/falcon/src/main/java/ai/picovoice/falcon/FalconNative.java
+++ binding/android/Falcon/falcon/src/main/java/ai/picovoice/falcon/FalconNative.java
@@ -1,5 +1,5 @@
 /*
-    Copyright 2024 Picovoice Inc.
+    Copyright 2024-2025 Picovoice Inc.
 
     You may not use this file except in compliance with the license. A copy of the license is
     located in the "LICENSE" file accompanying this source.
@@ -22,7 +22,8 @@ class FalconNative {
 
     static native long init(
             String accessKey,
-            String modelPath) throws FalconException;
+            String modelPath,
+            String device) throws FalconException;
 
     static native void delete(long object);
 
